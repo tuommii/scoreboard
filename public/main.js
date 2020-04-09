8a07e718ff4a464b176ced05906c400fa0a70ed7
Miikka Tuominen <tuommii@gmail.com>
2020-04-09 23:04:05 +0300

    add player to selectable list

--- public/main.js
+++ public/main.js
@@ -46,9 +46,30 @@ const startingData = {
 	players: ['Tiger King', 'Ying Jang']
 };
 
+// TODO: ignore case
+// TODO: Show error if same
+function isUniq(name, arr) {
+	code = true;
+	arr.forEach(player => {
+		console.log(name, player.name);
+
+		if (name === player.name)
+		{
+			console.log('SAMA');
+			code = false;
+			return code;
+		}
+	});
+	return code;
+}
+
 function addPlayer(e) {
 	e.preventDefault();
-	console.log('sas');
+	// this.playersArr.push(this.player);
+	if (this.player.length > 0 && isUniq(this.player, this.selectedPlayers)) {
+		this.selectedPlayers.push({name: this.player, selected: true});
+		this.player = '';
+	}
 }
 
 function toggleSelected(player) {
