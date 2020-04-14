aa4b6ae348de4e6830244c512ed90058af255cce
Miikka Tuominen <tuommii@gmail.com>
2020-04-14 23:37:09 +0300

    example component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Haloo ei ole nero!"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Haloo ei ole nero!"/> -->
+    <JoinForm />
     <button class="button">Klikkaa</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import JoinForm from './components/JoinForm.vue'
 import '../node_modules/bulma/css/bulma.min.css'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    JoinForm
   }
 }
 </script>
---
+++ frontend/src/components/JoinForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="form">
+    <div class="control">
+      ssasa
+      <input type="text" class="input" placeholder="ID" v-model="gameID">
+      <p class="help err">{{errors.join}}</p>
+    </div>
+    <div class="control">
+      <button class="button" @click="handleJoin">Join</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      gameID: 'SAsas',
+      errors: {
+        join: 'Game not found'
+      }
+    }
+  },
+  methods: {
+    handleJoin() {
+      console.log('Join!');
+    }
+  }
+}
+</script>
