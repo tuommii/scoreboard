4a8a42958578f9941167da15dac9d4c733628b01
Miikka Tuominen <tuommii@gmail.com>
2020-04-23 09:34:04 +0300

    testes for distance

---
+++ server/distance_test.go
@@ -0,0 +1,29 @@
+package server
+
+import "testing"
+
+func TestDistance(t *testing.T) {
+
+	taliLon := 24.846823667701187
+	taliLat := 60.21315136048605
+	hermanniLat := 60.195099
+	hermanniLon := 24.966789
+
+	woltLat := 60.1704502
+	woltLon := 24.929392
+
+	hiveLat := 60.180771
+	hiveLon := 24.9560864
+
+	dist := Distance(taliLat, taliLon, hermanniLat, hermanniLon)
+	wanted := 1.0
+	if dist != wanted {
+		t.Errorf("got: %f, wanted: %f", dist, wanted)
+	}
+
+	dist = Distance(hiveLat, hiveLon, woltLat, woltLon)
+	wanted = 1.0
+	if dist != wanted {
+		t.Errorf("got: %f, wanted: %f", dist, wanted)
+	}
+}
