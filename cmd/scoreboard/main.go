5b10be921de1fdf51673619e111cc219963bab8e
Miikka Tuominen <tuommii@gmail.com>
2020-05-02 16:17:30 +0300

    graceful shutdown

--- cmd/scoreboard/main.go
+++ cmd/scoreboard/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"miikka.xyz/scoreboard/server"
 )
@@ -12,6 +18,23 @@ func main() {
 	flag.Parse()
 	server := server.New(*dir)
 	go server.AutoClean()
-	log.Println("Starting server...")
-	server.HTTP.ListenAndServe()
+	go func() {
+		log.Println("Starting server...")
+		if err := server.HTTP.ListenAndServe(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	shutdown(server.HTTP)
+}
+
+func shutdown(server *http.Server) {
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	<-signalCh
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+	server.Shutdown(ctx)
+	log.Println("Exiting...")
+	os.Exit(0)
 }
