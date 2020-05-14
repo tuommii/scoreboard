352fb762e1016f37733ae6fac231a1698c9235a0
Miikka Tuominen <tuommii@gmail.com>
2020-05-14 08:02:59 +0300

    test create via api

--- server/server_test.go
+++ server/server_test.go
@@ -1,14 +1,18 @@
 package server
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
 	"net"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"miikka.xyz/scoreboard/game"
 )
 
-func TestHomeHandle(t *testing.T) {
+func TestServer(t *testing.T) {
 	l, err := net.Listen("tcp", "127.0.0.1:8080")
 	if err != nil {
 		log.Fatal(err)
@@ -16,27 +20,40 @@ func TestHomeHandle(t *testing.T) {
 	s := New("../")
 	server := httptest.NewUnstartedServer(s.HTTP.Handler)
 
-	// Change listener
 	server.Listener.Close()
 	server.Listener = l
 	server.Start()
 	defer server.Close()
 
-	resp, err := http.Get("http://localhost:8080/_status")
+	resp, err := http.Get(server.URL + "/_status")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if resp.StatusCode != 200 {
-		t.Errorf("WRONG STATUS: %d", resp.StatusCode)
+	wanted := http.StatusOK
+	if resp.StatusCode != wanted {
+		t.Errorf("WRONG STATUS! GOT: %d, WANTED: %d", resp.StatusCode, wanted)
 	}
 
-	resp, err = http.Get("http://localhost:8080/games/1")
+	resp, err = http.Get(server.URL + "/games/1")
 	if err != nil {
-		t.Fatal(err)
+		t.Errorf(err.Error())
+	}
+	if resp.StatusCode != http.StatusInternalServerError {
+		t.Errorf("WRONG STATUS! GOT: %d, WANTED: %d", resp.StatusCode, wanted)
 	}
 
-	if resp.StatusCode != 500 {
-		t.Errorf("WRONG STATUS: %d", resp.StatusCode)
+	basis := game.Basis{BasketCount: 2, Lat: 0, Lon: 0, Players: []string{"Miikka", "Pasi"}}
+	jsonStr, err := json.Marshal(basis)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	resp, err = http.Post(server.URL+"/games/create", "application/json", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("WRONG STATUS! GOT: %d, WANTED: %d", resp.StatusCode, wanted)
 	}
 }
